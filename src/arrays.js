0534063e1971e8e8e60b0c7784cc487cd02eb6f9
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2022-12-06 18:49:43 +0100

    First commit

---
+++ src/arrays.js
@@ -0,0 +1,121 @@
+
+const boxingQuestions = [
+  {
+    questionText: 'What is by far the most utilized punch in boxing?',
+    answerOptions: [
+      'The cross',
+      'The jab',
+      'The hook',
+    ],
+    correctAnswer: 'The jab',
+  },
+  {
+    questionText: 'When does a boxer become undisputed champion in his/her weightclass?',
+    answerOptions: [
+      'When she/he has aquired 50 wins in their weightclass',
+      'When she/he has aquired the IBF, WBO, WBA and WBC belts',
+      'When she/he has aquired the IBF, WBO, WBA, WBC and WTT belts',
+    ],
+    correctAnswer: 'When she/he has aquired the IBF, WBO, WBA and WBC belts',
+  },
+  {
+    questionText: 'What did former champion Vasyl Lomatjenkos dad make him do for 4 years before he could start training boxing?',
+    answerOptions: [
+      'Swimming, to give him an advantage in back strength, core strength and endurance',
+      'Ukranian folk dance, to give him an advantage in his footwork',
+      'Wrestling, to give him an advantage in overall strength and mental prowess',
+    ],
+    correctAnswer: 'Ukranian folk dance, to give him an advantage in his footwork',
+  },
+  {
+    questionText: 'Which two of these boxers were heavyweight champions of the world at one point in their career?',
+    answerOptions: [
+      'Muhammad Ali',
+      'Michael Bentt',
+      'Ron Lyle',
+    ],
+    correctAnswer: 'Muhammad Ali',
+    correctAnswer: 'Michael Bentt',
+  },
+];
+
+const muayThaiQuestions = [
+  {
+    questionText: 'What preferred offense does a "Muay khao" fighter use?',
+    answerOptions: [
+      'His/her knees',
+      'His/her hands',
+      'His/her elbows',
+    ],
+    correctAnswer: 'His/her knees',
+  },
+  {
+    questionText: 'Who is the only muay thai boxer who has won "Sports Writers of Thailand Fighter of the Year" three years in a row?',
+    answerOptions: [
+      'Samart Payakaroon, 1981 - 1983',
+      'Panpayak Jitmuangnon, 2013 - 2015',
+      'Petchboonchu FA Group, 2006 - 2008',
+    ],
+    correctAnswer: 'Panpayak Jitmuangnon, 2013-2015',
+  },
+  {
+    questionText: 'What two stadiums in Thailand is historically held with the highest regard when it comes to professional muay thai?',
+    answerOptions: [
+      'Rajadamnern stadium in Bangkok',
+      'Bangla stadium in Phuket',
+      'Lumpinee stadium in Bangkok',
+    ],
+    correctAnswer: 'Rajadamnern stadium in Bangkok',
+    correctAnswer: 'Lumpinee stadium in Bangkok',
+  },
+  {
+    questionText: 'What is a "Wai khru ram muay" and what does it represent?',
+    answerOptions: [
+      'An armband which is worn throughout the fight which honors your respective gym and khru (trainer)',
+      'A ritualistic dance which is performed before a fight to show respect and gratitude to the parents, khrus (trainers), ancestors and the ring itself',
+      'A fighters last fight which is often more of a showing/spectacle combined with a speech to honor said fighters journey, krhus (trainers), gym and fans',
+    ],
+    correctAnswer: 'A ritualistic dance which is performed before a fight to show respect and gratitude to the parents, khrus (trainers), ancestors and the ring itself',
+  },
+];
+
+const mmaQuestions = [
+  {
+    questionText: 'In what two weightclasses was irish MMA fighter Conor McGregor simultaneously champion of in the UFC?',
+    answerOptions: [
+      'Featherweight',
+      'Welterweight',
+      'Lightweight',
+    ],
+    correctAnswer: 'Lightweight',
+    correctAnswer: 'Featherweight',
+  },
+  {
+    questionText: 'Several high ranked MMA fighters and champions descend from which well-known wrestling optimistic russian republic?',
+    answerOptions: [
+      'Chechen Republic',
+      'Republic of Dagestan',
+      'Republic of Mordovia',
+    ],
+    correctAnswer: 'Republic of Dagestan',
+  },
+  {
+    questionText: 'Besides the UFC (Ultimate Fighting Championship), name two other well-known MMA organisations around the world',
+    answerOptions : [
+      'Bellator MMA',
+      'Tukhgul MMA Championship',
+      'Cage Warriors',
+    ],
+    correctAnswer: 'Bellator MMA',
+    correctAnswer: 'Cage Warriors',
+  },
+  {
+    questionText: 'Which fighter and champion has held the longest winningstreak ever in the UFC (Ultimate Fighting Championship)?',
+    answerOptions: [
+      'Tony Ferguson, lightweight: 2013 - 2020',
+      'Anderson Silva, middleweight, light heavyweight: 2006 - 2013',
+      'Demetrious Johnson, flyweight: 2012 - 2018',
+    ],
+    correctAnswer: 'Anderson Silva, middleweight, light heavyweight: 2006 - 2013'
+  },
+];
---
+++ src/main.js
@@ -0,0 +1,106 @@
+import './style/style.scss'; // Import SCSS
+import './arrays'; // Import arrays of questions
+
+
+const gameDescText = 'Welcome! Please choose your preferred quiz-topic!'; // 
+const gameDescription = document.querySelector('.gameDescription');
+
+gameDescription.innerHTML = gameDescText;
+
+document.querySelector('#startBoxingBtn').addEventListener('click', startGame); // Button for boxing quiz
+document.querySelector('#startMtBtn').addEventListener('click', startGame); // Button for muay thai quiz
+document.querySelector('#startMmaBtn').addEventListener('click', startGame); // Button for mma quiz
+
+let playerName = '';
+
+function startGame() {
+  playerName = document.querySelector('#playerInput').value; // Save name of player
+  
+  const boxingQuiz = document.querySelector('#boxingQuestionContainer'); // Quiz about boxing
+  const muayThaiQuiz = document.querySelector('#muayThaiQuestionContainer'); // Quiz about muay thai
+  const mmaQuiz = document.querySelector('#mmaQuestionContainer'); // Quiz about mma
+  
+  gameDescription.style.display = 'none'; // Hide HTML-elements
+  document.querySelector('#playerDetails').style.display = 'none'; // Hide HTML-elements
+
+  if (startBoxingBtn === 'clicked') { // Pair boxingquiz button with boxing quiz
+    boxingQuiz.startGame; // Start boxing quiz
+    document.querySelector('#startMtBtn').style.display = 'none'; // Hide muay thai quiz button when selected
+    document.querySelector('#startMmaBtn').style.display = 'none'; // Hide mma quiz button when selected
+  } else if (startMtBtn === 'clicked') { // Pair muay thai quiz button with muay thai quiz
+    muayThaiQuiz.startGame; // Start muay thai quiz
+    document.querySelector('#startBoxingBtn').style.display = 'none'; // Hide boxing quiz button when selected
+    document.querySelector('#startMmaBtn').style.display = 'none'; // Hide mma quiz button when selected
+  } else if (startMmaBtn === 'clicked') { // Pair mma quiz button with mma quiz
+    mmaQuiz.startGame; // Start mma quiz
+    document.querySelector('#startBoxingBtn').style.display = 'none'; // Hide boxing quiz button when selected
+    document.querySelector('#startMtBtn').style.display = 'none'; // Hide muay thai quiz button when selected
+  };
+
+  nextQuestion();
+}
+
+const questionTextDiv = document.querySelector('#questionText');
+const answer1Btn = document.querySelector('#answer1');
+const answer2Btn = document.querySelector('#answer2');
+const answer3Btn = document.querySelector('#answer3');
+
+answer1Btn.addEventListener('click', checkAnswer);
+answer2Btn.addEventListener('click', checkAnswer);
+answer3Btn.addEventListener('click', checkAnswer);
+
+let currentQuestion = 0;
+let points = 0;
+
+function checkAnswer(e) {
+  const userAnswer = e.currentTarget.innerHTML; 
+
+  const correctAnswer = questions[currentQuestion - 1].correctAnswer;
+
+  if (userAnswer === correctAnswer) { // Compare the players answer to correct answer
+    points++; // 1 point given
+  } else {
+    points -1; // 1 point deducted
+  }
+  nextQuestion();
+}
+
+function nextQuestion() {
+  if (currentQuestion >= questions.length) { // > =
+    gameOver();
+    return;
+  }
+
+  questionTextDiv.innerHTML = questions[currentQuestion].questionText;
+  answer1Btn.innerHTML = questions[currentQuestion].answerOptions[0];
+  answer2Btn.innerHTML = questions[currentQuestion].answerOptions[1];
+  answer3Btn.innerHTML = questions[currentQuestion].answerOptions[2];
+
+  currentQuestion++; // += 1, currentQuestion = currentQuestion + 1;
+}
+
+document.querySelector('#restartBtn').addEventListener('click', restartGame);
+document.querySelector('#startDifferentQuiz').addEventListener('click', startAnotherQuiz)
+
+function restartGame() {
+  document.querySelector('#gameOver').style.display = 'none';
+  document.querySelector('#questionContainer').classList.remove('hidden');
+  currentQuestion = 0;
+  points = 0;
+  nextQuestion();
+}
+
+function startAnotherQuiz() {
+  document.querySelector('#gameOver').style.display = 'none';
+  document.querySelector('#questionContainer').classList.remove('hidden');
+  currentQuestion = 0;
+  points = 0;
+  nextQuestion();
+}
+
+function gameOver() {
+  document.querySelector('#gameOver').style.display = 'block';
+  document.querySelector('#questionContainer').classList.add('hidden');
+  document.querySelector('#pointsContainer').innerHTML = `Du fick ${points} poäng!`;
+}
+
