85d62aa17af73a984edf6ff84d6136ef46c80e6e
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2022-12-20 18:19:48 +0100

    Försök till att komprimera kod + filtrera frågearray - Handledning onsdag

--- src/main.js
+++ src/main.js
@@ -1,74 +1,82 @@
 import './style/style.scss'; // Import SCSS
-import './arrays'; // Import arrays of questions
+import { questions } from './arrays';
 
 
-const gameDescText = 'Welcome! Please choose your preferred quiz-topic!'; // 
-const gameDescription = document.querySelector('.gameDescription');
-
-gameDescription.innerHTML = gameDescText;
-
-document.querySelector('#startBoxingBtn').addEventListener('click', startGame); // Button for boxing quiz
-document.querySelector('#startMtBtn').addEventListener('click', startGame); // Button for muay thai quiz
-document.querySelector('#startMmaBtn').addEventListener('click', startGame); // Button for mma quiz
-
-let playerName = '';
-
-function startGame() {
-  playerName = document.querySelector('#playerInput').value; // Save name of player
-  
-  const boxingQuiz = document.querySelector('#boxingQuestionContainer'); // Quiz about boxing
-  const muayThaiQuiz = document.querySelector('#muayThaiQuestionContainer'); // Quiz about muay thai
-  const mmaQuiz = document.querySelector('#mmaQuestionContainer'); // Quiz about mma
-  
-  gameDescription.style.display = 'none'; // Hide HTML-elements
-  document.querySelector('#playerDetails').style.display = 'none'; // Hide HTML-elements
-
-  if (startbxngbtn === 'click') { // Pair boxingquiz button with boxing quiz
-    boxingQuiz.startGame; // Start boxing quiz
-    document.querySelector('#startmtbtn').style.display = 'none'; // Hide muay thai quiz button when selected
-    document.querySelector('#startmmabtn').style.display = 'none'; // Hide mma quiz button when selected
-  } else if (startmtbtn === 'click') { // Pair muay thai quiz button with muay thai quiz
-    muayThaiQuiz.startGame; // Start muay thai quiz
-    document.querySelector('#startbxngbtn').style.display = 'none'; // Hide boxing quiz button when selected
-    document.querySelector('#startmmabtn').style.display = 'none'; // Hide mma quiz button when selected
-  } else if (startmmabtn === 'click') { // Pair mma quiz button with mma quiz
-    mmaQuiz.startGame; // Start mma quiz
-    document.querySelector('#startbxngbtn').style.display = 'none'; // Hide boxing quiz button when selected
-    document.querySelector('#startmtbtn').style.display = 'none'; // Hide muay thai quiz button when selected
-  };
-
-  nextQuestion();
-}
-
+const gameDescText = 'Welcome! Please choose your preferred quiz-topic and enter your name!'; // Game description when you arrive
+const gameDescription = document.querySelector('#gameDescription');
 const questionTextDiv = document.querySelector('#questionText');
 const answer1Btn = document.querySelector('#answer1');
 const answer2Btn = document.querySelector('#answer2');
 const answer3Btn = document.querySelector('#answer3');
+const shuffled = questions.sort(() => Math.random() - 0.5) // Variable for shuffling array questions
+
+let boxingQuestions = questions.filter(boxingCategory) // Fråga jenni om hjälp!
+let muaythaiQuestions = questions.filter(muaythaiCategory)
+let mmaQuestions = questions.filter(mmaCategory)
+
+gameDescription.innerHTML = gameDescText;
+
+document.querySelector('#startGameBtn').addEventListener('click', startQuiz); // Button for starting quiz
+document.querySelector('#restartBtn').addEventListener('click', restartQuiz); // Button for restarting quiz
 
 answer1Btn.addEventListener('click', checkAnswer);
 answer2Btn.addEventListener('click', checkAnswer);
 answer3Btn.addEventListener('click', checkAnswer);
 
+
+let playerName = '';
 let currentQuestion = 0;
 let points = 0;
 
+
+function boxingCategory(category) { // Fråga jenni om hjälp
+  if (document.getElementById('boxing').checked) {
+    return category === 'Boxing';
+  }
+}
+
+function muaythaiCategory(category) {
+  if (document.getElementById('muaythai').checked) {
+    return category === 'Muay thai';
+  }
+}
+
+function mmaCategory(category) {
+  if (document.getElementById('mma').checked) {
+    return category === 'MMA';
+  }
+}
+
+
+function startQuiz() {
+  playerName = document.querySelector('#playerInput').value; // Saving players name
+
+  gameDescription.style.display = 'none'; // Hide HTML
+  document.querySelector('#playerDetails').style.display = 'none';
+
+
+  shuffled();
+  nextQuestion();
+}
+
+
 function checkAnswer(e) {
-  const userAnswer = e.currentTarget.innerHTML; 
+  const userAnswer = e.currentTarget.innerHTML;
 
   const correctAnswer = questions[currentQuestion - 1].correctAnswer;
-
-  if (userAnswer === correctAnswer) { // Compare the players answer to correct answer
-    points++; // 1 point given
-  } else {
-    points -=1; // 1 point deducted
+  if (userAnswer == correctAnswer) {
+      points++;
+  } else if (userAnswer != correctAnswer) {
+    points--;
   }
+
   nextQuestion();
 }
 
 function nextQuestion() {
-  if (currentQuestion >= questions.length) { // If there is no more questions - game over
-    gameOver();
-    return;
+  if (currentQuestion >= questions.length) {
+      gameOver();
+      return;
   }
 
   questionTextDiv.innerHTML = questions[currentQuestion].questionText;
@@ -76,32 +84,25 @@ function nextQuestion() {
   answer2Btn.innerHTML = questions[currentQuestion].answerOptions[1];
   answer3Btn.innerHTML = questions[currentQuestion].answerOptions[2];
 
-  currentQuestion++; // += 1, currentQuestion = currentQuestion + 1;
+  currentQuestion++;
 }
 
-document.querySelector('#restartBtn').addEventListener('click', restartGame);
-document.querySelector('#startDifferentQuiz').addEventListener('click', startAnotherQuiz)
 
-function restartGame() {
+function restartQuiz() {
   document.querySelector('#gameOver').style.display = 'none';
   document.querySelector('#questionContainer').classList.remove('hidden');
   currentQuestion = 0;
-  points = 0;
   nextQuestion();
+  points = 0;
 }
 
-function startAnotherQuiz() {
-  document.querySelector('#gameOver').style.display = 'none';
-  document.querySelector('#questionContainer').classList.remove('hidden');
-  currentQuestion = 0;
-  points = 0;
-  nextQuestion();
-}
+
 
 function gameOver() {
   document.querySelector('#gameOver').style.display = 'block';
   document.querySelector('#questionContainer').classList.add('hidden');
-  document.querySelector('#pointsContainer').innerHTML = `Du fick ${points} poäng!`;
+  document.querySelector('#pointsContainer').innerHTML = `Grattis, ${playerName}, du fick ${points} poäng!`;
 }
+
 
 
