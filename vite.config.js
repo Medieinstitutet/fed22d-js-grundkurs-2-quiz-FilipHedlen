6ed73a4314fdc3af8d017be10fb5659bda219dc5
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2022-12-30 22:03:14 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite';
 
-/*
-Om ditt repo har adressen https://github.com/Medieinstitutet/fed22d-js-grundkurs-jenni-wumpus,
-då ska "base" här nedan vara "/fed22d-js-grundkurs-jenni-wumpus/"
- */
 
 export default defineConfig({
   base: '/fed22d-js-grundkurs-2-quiz-FilipHedlen/', // TODO - ändra till ditt repo-namn
